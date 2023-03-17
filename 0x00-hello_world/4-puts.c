ebcdda960b88d815fca58b449277beda8d7e972d
abdalrahmanstar <alswidyabdalrahman12@gmail.com>
2023-03-16 20:08:17 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+*main - main entery poit
+*return : always 9 (success)
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle")
-	return 0
+	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

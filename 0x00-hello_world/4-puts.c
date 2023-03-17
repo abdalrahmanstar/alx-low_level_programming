3307a6d304a5236a21b1d96b54f4132596a6d1c2
abdalrahmanstar <alswidyabdalrahman12@gmail.com>
2023-03-16 20:41:55 -0700

    Size is not grandeur

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
-*main - main entery poit
-*return : always 9 (success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
